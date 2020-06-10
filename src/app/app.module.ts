f6e738c3b114d7c3190f6e8bb28fb2c13b775fa1
bhushankumar-developer <bhushankumar.delicate@gmail.com>
2020-06-10 11:17:48 +0530

    3 files modified Translation issue solve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
-
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { ExamplesModule } from './features/examples.module';
+import { TranslateModule ,TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { environment } from '../environments/environment';
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
 
+}
 @NgModule({
   imports: [
     // angular
     BrowserAnimationsModule,
     BrowserModule,
-
+     TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    HttpClientModule,
     // core
     CoreModule,
     ExamplesModule,
